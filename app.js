468bb560ccaffba4f55d58d47bad7fda5268adca
Suresh Gopi <sgtulam@gmail.com>
2022-04-04 19:35:45 -0500

    added addmods module

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var elementRouter = require('./routes/element');
+var addModsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/element', elementRouter);
+app.use('/addmods', addModsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
